b864cd329f5b703932d01f7fd0dc8592dc236d43
liushoukun <liushoukun66@gmail.com>
2025-01-24 11:35:41 +0800

    feat(user): 实现用户登录服务管理器
    - 新增 ServiceManager 基类，用于管理服务提供者
    - 创建 UserLoginServiceProviderManager，管理用户登录服务提供者
    - 添加 UserLoginService 用于处理用户登录逻辑
    - 实现 PasswordLoginServiceProvider、SmsLoginServiceProvider 和 SocialiteLoginServiceProvider
    - 更新配置文件，添加服务管理器相关配置

---
+++ src/Helpers/Services/ServiceManager.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace RedJasmine\Support\Helpers\Services;
+
+use Closure;
+use Illuminate\Support\Arr;
+use function strtolower;
+
+abstract class ServiceManager
+{
+
+    protected array $config;
+    protected array $resolved = [];
+    protected const PROVIDERS = [];
+
+    public function __construct(array $config)
+    {
+    }
+
+    public function getConfig() : array
+    {
+        return $this->config;
+    }
+
+    public function setConfig(array $config) : ServiceManager
+    {
+        $this->config = $config;
+        return $this;
+    }
+
+    protected static array $customCreators = [];
+
+    public function extend(string $name, Closure $callback) : self
+    {
+        static::$customCreators[strtolower($name)] = $callback;
+
+        return $this;
+    }
+
+    public function create(string $name)
+    {
+        $name = strtolower($name);
+
+        if (!isset($this->resolved[$name])) {
+            $this->resolved[$name] = $this->createProvider($name);
+        }
+
+        return $this->resolved[$name];
+    }
+
+    protected function createProvider(string $name)
+    {
+        $config   = Arr::get($this->config, $name, []);
+        $provider = $config['provider'] ?? $name;
+
+        if (isset(self::$customCreators[$provider])) {
+            return $this->callCustomCreator($provider, $config);
+        }
+
+        if (!$this->isValidProvider($provider)) {
+            throw new Exceptions\InvalidArgumentException("Provider [{$name}] not supported.");
+        }
+
+        return $this->buildProvider(static::PROVIDERS[$provider] ?? $provider, $config);
+    }
+
+    public function getResolvedProviders() : array
+    {
+        return $this->resolved;
+    }
+
+    public function buildProvider(string $provider, array $config)
+    {
+        return new $provider($config);
+
+    }
+
+    protected function callCustomCreator(string $name, array $config)
+    {
+        return self::$customCreators[$name]($config);
+    }
+
+    protected function isValidProvider(string $provider) : bool
+    {
+        return isset(static::PROVIDERS[$provider]);
+    }
+}
